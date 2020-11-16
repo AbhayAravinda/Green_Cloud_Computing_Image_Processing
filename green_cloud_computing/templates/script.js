eefec2ce8b8df0498318c8ec78f1fef4475998c0
Aayush Mishra <48150464+mishra-aayush@users.noreply.github.com>
2020-11-16 09:22:10 +0530

    Add files via upload

---
+++ green_cloud_computing/templates/script.js
@@ -0,0 +1,17 @@
+function readURL(input) 
+{
+	if (input.files && input.files[0]) 
+	{
+		var reader = new FileReader();
+
+		reader.onload = function (e) 
+		{
+			$('#image')
+			.attr('src', e.target.result);
+		};
+		reader.readAsDataURL(input.files[0]);
+	}
+	var x = document.getElementById("image");
+
+	x.style.display = "block";
+}
