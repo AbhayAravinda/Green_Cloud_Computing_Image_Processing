881bf8d374bd121a02937a75294668cd52ed7173
Aayush Mishra <48150464+mishra-aayush@users.noreply.github.com>
2020-11-18 18:55:43 +0530

    image to base-64 and effect to json object

--- static/script.js
+++ static/script.js
@@ -17,3 +17,32 @@ function readURL(input)
 	var y = document.getElementById("para");
 	y.style.display="none";
 }
+
+var result = {};
+
+function final() { 
+	selectElement =  
+	document.querySelector('#imgUpload');
+	output = selectElement.value; 
+	result['effect'] = output;
+	console.log(result);
+
+    $.post('/upload-image/', result, function(response){
+        if(response === 'success'){ alert('Yay!'); }
+        else{ alert('Error! :('); }});
+}
+
+function getDataUrl(img)
+{
+   const canvas = document.createElement('canvas');
+   const ctx = canvas.getContext('2d');
+   canvas.height = img.naturalHeight;
+   canvas.width = img.naturalWidth;
+   ctx.drawImage(img, 0, 0);
+   return canvas.toDataURL('image/jpeg');
+}
+const img = document.querySelector('#image');
+img.addEventListener('load', function (event) {
+   const dataUrl = getDataUrl(event.currentTarget);
+   result['image'] = dataUrl;
+});
