4a6de1db7ead06e1f785b143bb260174f1b5ca49
Aayush Mishra <48150464+mishra-aayush@users.noreply.github.com>
2020-11-20 11:45:58 +0530

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,5 +1,3 @@
-//TODO: Try to get rid of jQuery
-
 function readURL(input) 
 {
 	if (input.files && input.files[0]) 
@@ -19,48 +17,3 @@ function readURL(input)
 	var y = document.getElementById("para");
 	y.style.display="none";
 }
-
-var result = {};
-
-function final() { 
-	selectElement =  
-	document.querySelector('#imgUpload');
-	output = selectElement.value; 
-	result['process'] = output;
-	console.log(result);
-
- 
-	const csrftoken = document.querySelector('[name=csrfmiddlewaretoken]').value;
- 	const request = new Request(
-	    '/upload_image',
-	    {headers: {'X-CSRFToken': csrftoken}}
-	);
-	fetch(request, {
-	    method: 'POST',
-	    mode: 'same-origin',  // Do not send CSRF token to another domain.
-	    body: JSON.stringify(result)
-	}).then(function(response) {
-	    return response.json()
-	}).then(function(json) {
-		processed_image_url = json.processed_image_url
-		// TODO: Instead of redirecting, choose what you want to do
-		window.location = processed_image_url
-	})
-
-
-}
-
-function getDataUrl(img)
-{
-   const canvas = document.createElement('canvas');
-   const ctx = canvas.getContext('2d');
-   canvas.height = img.naturalHeight;
-   canvas.width = img.naturalWidth;
-   ctx.drawImage(img, 0, 0);
-   return canvas.toDataURL('image/jpeg');
-}
-const img = document.querySelector('#image');
-img.addEventListener('load', function (event) {
-   const dataUrl = getDataUrl(event.currentTarget);
-   result['base64_image'] = dataUrl;
-});
