43d0e2429a102ab87e4c0ccbc49f068c85e6ad3e
Abhay Aravinda <17ucs002@lnmiit.ac.in>
2020-11-21 00:44:40 +0530

    Replaced SQl with Mongo

--- green_cloud_computing/settings.py
+++ green_cloud_computing/settings.py
@@ -78,8 +78,8 @@ WSGI_APPLICATION = 'green_cloud_computing.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'djongo',
+        'NAME': 'green_cloud_computing_db',
     }
 }
 
